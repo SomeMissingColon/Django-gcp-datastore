745a4f3808100c6ac0a3a48ecbfcd5e230e079ab
Thomas Dion <thomasnoidion@gmail.com>
2022-03-14 16:48:26 -0400

    added gcloud functionality

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4n1*%#yts*!2dq*$7)eb)%9(33u=x8-(^sw4ru!cq6cgeu*brn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',0)))
 
 ALLOWED_HOSTS = ['*']
 
